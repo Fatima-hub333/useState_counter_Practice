c7b55431192f527a5da9b3680235702686746e8a
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-21 23:48:07 +0500

    Update.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './index.css';
 
 export default function App() {
   const [count, setCount] = React.useState(0)
+  /**
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
+     */
   function add() {
-    setCount(count + 1)
+    setCount(prevCount => prevCount + 1) 
   }
   function minus() {
-    setCount(count - 1)
+    setCount(prevCount => prevCount - 1)
   }
   return (
     <div className="counter">
